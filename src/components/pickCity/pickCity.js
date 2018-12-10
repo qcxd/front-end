2ded4832655eef5a31f635e6351c872043973045
鄢旺 <yanwang508@pingan.com.cn>
2018-12-10 15:31:51 +0800

    锚点

--- src/components/pickCity/pickCity.js
+++ src/components/pickCity/pickCity.js
@@ -4,17 +4,21 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    currentCity: {
+      type: String,
+      value: ''
+    },
     cityList: {
       type: Array,
       value: []
-    }
+    },
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    currentView: '',
   },
 
   /**
@@ -22,13 +26,18 @@ Component({
    */
   methods: {
     doSelsct(e) {
-      console.log(e);
       let myEventDetail = { 
         id: e.currentTarget.dataset.id,
         name: e.currentTarget.dataset.name,
       }; // detail对象，提供给事件监听函数
       let myEventOption = {}; // 触发事件的选项
       this.triggerEvent('pickevent', myEventDetail, myEventOption)
+    },
+
+    jumpToView(e) {
+      this.setData({
+        currentView: e.currentTarget.dataset.code
+      })
     }
   }
 })
