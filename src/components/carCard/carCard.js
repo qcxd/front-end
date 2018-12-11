27ec814e8302053f3eb06ce1dda63aca6e5aa947
鄢旺 <yanwang508@pingan.com.cn>
2018-12-11 18:42:09 +0800

    fix

--- src/components/carCard/carCard.js
+++ src/components/carCard/carCard.js
@@ -4,7 +4,10 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    carDatail: {
+      type: Object,
+      value: {}
+    }
   },
 
   /**
@@ -18,6 +21,5 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
   }
 })
