11eec46aad6a8b0e1c631ad6a54466df06d7f4e7
鄢旺 <yanwang508@pingan.com.cn>
2018-12-20 17:19:31 +0800

    fix

--- src/pages/my-home/my-home.js
+++ src/pages/my-home/my-home.js
@@ -1,18 +1,22 @@
 // pages/my-home/my-home.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    user: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      user: app.globalData.userInfo
+    })
   },
 
   /**
