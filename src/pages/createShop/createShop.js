60d95ea8a2a6af7c74f048fa9bb0bdf029e470ec
Lisa5 <1348894516@qq.com>
2019-01-06 22:00:00 +0800

    fix

--- src/pages/createShop/createShop.js
+++ src/pages/createShop/createShop.js
@@ -9,7 +9,11 @@ Page({
     area: 2719,
     introduce: '',
     address: '',
-    logo: ''
+    logo: '',
+    filePath: "",
+    images: [],
+    uploadImgs: [],
+    count: 9
   },
 
   onLoad: function (options) {
@@ -22,6 +26,52 @@ Page({
   },
   /**  reset */
   formReset(e) {
+
+  },
+
+
+  chooseImage: function (e) {
+    var selectPictureNum = e.target.dataset.num;
+    this.setData({
+      count: 9 - selectPictureNum
+    })
+    var that = this;
+    wx.chooseImage({
+      count: that.data.count, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths;
+        that.setData({
+          filePath: res.tempFilePaths[0],
+          images: that.data.images.concat(tempFilePaths),
+          uploadImgs: res.tempFilePaths
+        })
+      },
+    })
+
+  },
+
+  uploadImg: function () {
+    var that = this;
+    console.log(JSON.stringify(that.data.uploadImgs))
+    for (var i = 0; i < that.data.uploadImgs.length; i++) {
+      var filePath = that.data.uploadImgs[i];
+
+      uploadImage(
+        {
+          filePath: filePath,
+          dir: "images/",
+          success: function (res) {
+            console.log("上传成功")
+            console.log("上传成功" + JSON.stringify(res))
+          },
+          fail: function (res) {
+            console.log("上传失败")
+            console.log(res)
+          }
+        })
+    }
 
   },
 
