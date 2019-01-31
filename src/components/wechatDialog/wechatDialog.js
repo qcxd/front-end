cf0f2cbb84a16b7b82f3eaeb8cf0e64273a77fce
鄢旺 <yanwang508@pingan.com.cn>
2019-01-31 16:45:07 +0800

    fix

--- src/components/wechatDialog/wechatDialog.js
+++ src/components/wechatDialog/wechatDialog.js
@@ -20,6 +20,35 @@ Component({
   methods: {
     closeWechat() {
       this.triggerEvent('closeWechat', {}, {})
-    }
+    },
+
+    /**  保存微信二维码 */
+    saveWechat() {
+      const _this = this;
+      wx.downloadFile({
+        url: 'https://example.com/audio/123',
+        success(res) {
+          // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
+          if (res.statusCode === 200) {
+            _this.saveImg(res.tempFilePath);
+          }
+        }
+      })
+    },
+
+    /** 保存临时文件 */
+    saveImg(filePath) {
+      wx.saveImageToPhotosAlbum({
+        filePath: filePath, // 图片文件路径，可以是临时文件路径或永久文件路径，不支持网络图片路径
+        success(res) {
+           wx.showToast({
+            title: '已保存到系统相册',
+          })
+        },
+        file(err) {
+          showModal();
+        }
+      })
+    },
   }
 })
